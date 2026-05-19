85514e8fe773d862e4381d3380c2c0253c6d2c92
Bogdan Cotinghiu <110598161+Bogdy70@users.noreply.github.com>
2026-05-19 12:01:33 +0300

    Refactor Matrix class into separate files
    Moved Matrix class from NeuralNetwork.cpp to Matrix.cpp/.h for better modularity. Updated project files to include and organize new Matrix source and header files. NeuralNetwork.cpp now includes Matrix.h. This improves code organization and maintainability.

---
+++ NeuralNetwork/Matrix.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include<vector>
+#include<string>
+
+class Matrix
+{
+private:
+    int rows;
+    int cols;
+    std::vector<float> data;
+
+public:
+    Matrix();
+
+    Matrix(int r, int c);
+
+    float& operator()(int row, int col);
+
+    const float& operator()(int row, int col) const;
+
+    int getRows() const;
+
+    int getCols() const;
+
+    void setData(const std::vector<float>& X);
+
+    int size() const;
+
+    void resize(int new_rows, int new_cols);
+
+    void reshape(int new_rows, int new_cols);
+
+    Matrix matmul(const Matrix& B) const;
+
+    static Matrix loadMatrixBin(const std::string& filepath, int rows, int cols);
+
+    Matrix operator*(const Matrix& B) const;
+
+    Matrix operator*(float x) const;
+
+    Matrix operator/(const Matrix& B) const;
+
+    Matrix operator/(float x) const;
+
+    Matrix operator+(const Matrix& B) const;
+
+    Matrix operator+(float x) const;
+
+    Matrix operator-(const Matrix& B) const;
+
+    Matrix operator-(float x) const;
+
+    Matrix operator==(const Matrix& B) const;
+
+    Matrix operator>(float x) const;
+
+    Matrix broadcastAdd(const Matrix& B) const;
+
+    Matrix T() const;
+
+    static Matrix random(const int rows, const int cols);
+
+    static Matrix sum(const Matrix& A, int axis = -1);
+
+    static Matrix powM(const Matrix& A, float power);
+
+    static Matrix sqrtM(const Matrix& A);
+
+    static Matrix expM(const Matrix& A);
+
+    static Matrix logM(const Matrix& A);
+
+    static Matrix maxM(const Matrix& A, int axis = -1);
+
+    static Matrix argmax(const Matrix& A, int axis = 0);
+
+    static Matrix clipM(const Matrix& A, float minValue, float maxValue);
+};
+
+Matrix operator*(float x, const Matrix& A);
+
+Matrix operator+(float x, const Matrix& A);
+
+Matrix operator/(float x, const Matrix& A);
+
+Matrix operator-(float x, const Matrix& A);
